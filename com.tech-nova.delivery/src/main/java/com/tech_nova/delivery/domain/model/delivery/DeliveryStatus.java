c1c1822edcef13d129c3f4cd22e4c33f3c302454
misu <misuhan33@gmail.com>
2024-12-11 00:35:22 +0900

    feat: edit DeliveryStatus

---
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/domain/model/delivery/DeliveryStatus.java
@@ -0,0 +1,18 @@
+package com.tech_nova.delivery.domain.model.delivery;
+
+public enum DeliveryStatus {
+    HUB_WAITING,
+    HUB_MOVING,
+    HUB_ARRIVE,
+    DELIVERY_ON,
+    DELIVERY_COMPLETED;
+
+    public static DeliveryStatus fromString(String status) {
+        for (DeliveryStatus deliveryStatus : DeliveryStatus.values()) {
+            if (deliveryStatus.name().equalsIgnoreCase(status)) {
+                return deliveryStatus;
+            }
+        }
+        throw new IllegalArgumentException("Unknown role: " + status);
+    }
+}
