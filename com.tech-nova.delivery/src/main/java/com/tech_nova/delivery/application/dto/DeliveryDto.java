5aab2ab02b2d640f6b0aadc14ea84e0b04bd8947
misu <misuhan33@gmail.com>
2024-12-12 01:00:33 +0900

    feat: edit Delivery
    - 수령업체아이디 추가

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/application/dto/DeliveryDto.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/application/dto/DeliveryDto.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class DeliveryDto {
     private UUID orderId;
+    private UUID recipientCompanyId;
     private String province;
     private String city;
     private String district;
@@ -17,6 +18,7 @@ public class DeliveryDto {
 
     public static DeliveryDto create(
             UUID orderId,
+            UUID recipientCompanyId,
             String province,
             String city,
             String district,
@@ -24,6 +26,7 @@ public class DeliveryDto {
     ) {
         return DeliveryDto.builder()
                 .orderId(orderId)
+                .recipientCompanyId(recipientCompanyId)
                 .province(province)
                 .city(city)
                 .district(district)
