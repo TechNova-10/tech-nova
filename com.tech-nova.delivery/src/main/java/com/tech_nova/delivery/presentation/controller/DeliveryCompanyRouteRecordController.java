99e846b05f234bcd2b0f0dfd43476c62c582a0d5
misu <misuhan33@gmail.com>
2024-12-12 14:04:35 +0900

    refactor: change return type from ApiResponseDto to ResponseEntity<ApiResponseDto<T>>

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryCompanyRouteRecordController.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryCompanyRouteRecordController.java
@@ -3,6 +3,7 @@ package com.tech_nova.delivery.presentation.controller;
 import com.tech_nova.delivery.application.service.DeliveryService;
 import com.tech_nova.delivery.presentation.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,10 +19,8 @@ public class DeliveryCompanyRouteRecordController {
     private final DeliveryService deliveryService;
 
     @PostMapping
-    public ApiResponseDto<Void> createCompanyRouteRecord(@RequestParam("delivery_id") UUID deliveryId) {
+    public ResponseEntity<ApiResponseDto<Void>> createCompanyRouteRecord(@RequestParam("delivery_id") UUID deliveryId) {
         deliveryService.createCompanyRouteRecordByDeliveryId(deliveryId);
-        return ApiResponseDto.success("Delivery route status updated successfully");
+        return ResponseEntity.ok(ApiResponseDto.success("Delivery route status updated successfully"));
     }
-
-
 }
--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryRouteRecordController.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryRouteRecordController.java
@@ -3,6 +3,7 @@ package com.tech_nova.delivery.presentation.controller;
 import com.tech_nova.delivery.application.service.DeliveryService;
 import com.tech_nova.delivery.presentation.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -15,8 +16,8 @@ public class DeliveryRouteRecordController {
     private final DeliveryService deliveryService;
 
     @PatchMapping("/{delivery_route_id}/status")
-    public ApiResponseDto<UUID> updateDeliveryRouteStatus(@PathVariable("delivery_route_id") UUID deliveryRouteId, @RequestParam("current_status") String currentStatus) {
+    public ResponseEntity<ApiResponseDto<UUID>> updateDeliveryRouteStatus(@PathVariable("delivery_route_id") UUID deliveryRouteId, @RequestParam("current_status") String currentStatus) {
         UUID routeRecordId = deliveryService.updateRouteRecordState(deliveryRouteId, currentStatus);
-        return ApiResponseDto.success("Delivery route status updated successfully", routeRecordId);
+        return ResponseEntity.ok(ApiResponseDto.success("Delivery route status updated successfully", routeRecordId));
     }
 }
