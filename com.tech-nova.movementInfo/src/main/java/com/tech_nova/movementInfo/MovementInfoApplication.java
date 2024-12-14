ffbfc7cd3bc37277ad7190827f3d8ba8fe81756d
wooseok <wooseok6008@gmail.com>
2024-12-14 17:15:18 +0900

    feat: 허브간 이동정보 생성 api 구현
    - Hub and Spoke 구현
    - 중앙 허브 관리를 하드코딩으로 구현완료 이후에 확장성 있도록 수정할 예정입니다. 일단 배송쪽에 정보 넘겨드려야 해서 구현해 놓았어요!

--- com.tech-nova.movementInfo/src/main/java/com/tech_nova/movementInfo/MovementInfoApplication.java
+++ com.tech-nova.movementInfo/src/main/java/com/tech_nova/movementInfo/MovementInfoApplication.java
@@ -2,7 +2,9 @@ package com.tech_nova.movementInfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
+@FeignClient
 @SpringBootApplication
 public class MovementInfoApplication {
 
