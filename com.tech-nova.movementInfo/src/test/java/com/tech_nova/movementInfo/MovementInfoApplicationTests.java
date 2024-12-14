bdc572d03c5246115859680e4b9372a5b02d87dd
wooseok <wooseok6008@gmail.com>
2024-12-14 16:55:36 +0900

    feat: movementInfo 서버 생성

---
+++ com.tech-nova.movementInfo/src/test/java/com/tech_nova/movementInfo/MovementInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_nova.movementInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
