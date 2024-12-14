4acb283f5bd5f378de68c325b2144b08ca487901
wooseok <wooseok6008@gmail.com>
2024-12-14 16:55:36 +0900

    feat: movementInfo 서버 생성

---
+++ com.tech-nova.movementInfo/src/main/java/com/tech_nova/movementInfo/MovementInfoApplication.java
@@ -0,0 +1,13 @@
+package com.tech_nova.movementInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementInfoApplication.class, args);
+	}
+
+}
