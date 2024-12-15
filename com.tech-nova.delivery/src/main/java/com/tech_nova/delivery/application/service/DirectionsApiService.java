acb286be292359aae452fc18a811aba5a432b291
misu <misuhan33@gmail.com>
2024-12-16 06:50:39 +0900

    feat: 배송 담당자에게 보낼 슬랙 메시지 생성 기능 완성
    - Naver Directions5 API 이용해 총 배송 예상 시간과 거리 추출
    - 슬랙 메시지 생성 예시
    1번 배송지: 부산광역시 부산 부산진구 백양순환로119번길 62
    2번 배송지: 부산광역시 부산 부산진구 가야대로 772
    3번 배송지: 부산광역시 부산 부산진구 전포대로255번길 37
    4번 배송지: 부산광역시 부산 부산진구 신천대로 241
    예상 거리: 9.64 km, 예상 시간: 31 분

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/application/service/DirectionsApiService.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/application/service/DirectionsApiService.java
@@ -5,5 +5,5 @@ import com.tech_nova.delivery.application.dto.RouteEstimateData;
 public interface DirectionsApiService {
     RouteEstimateData getRouteEstimateData(String start, String goal);
 
-    void getWaypointsSequence(String start, String goal, String waypoints);
+    RouteEstimateData getRouteEstimateData(String start, String goal, String waypoints);
 }
