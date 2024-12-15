c58a18a9059c4eb379b01a5f41f1613951af3d8d
misu <misuhan33@gmail.com>
2024-12-15 14:31:19 +0900

    feat : CompanyClient 수정
    - 호출 api 추가

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/infrastructure/client/CompanyClient.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/infrastructure/client/CompanyClient.java
@@ -1,8 +1,16 @@
 package com.tech_nova.delivery.infrastructure.client;
 
 import com.tech_nova.delivery.application.service.CompanyService;
+import com.tech_nova.delivery.infrastructure.dto.CompanyResponse;
+import com.tech_nova.delivery.presentation.dto.ApiResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 @FeignClient(name = "company-service", url = "http://auth-service/api/v1/companies")
 public interface CompanyClient extends CompanyService {
+    @GetMapping("/{companyId}")
+    ApiResponseDto<CompanyResponse> getCompanyById(@PathVariable UUID companyId);
 }
