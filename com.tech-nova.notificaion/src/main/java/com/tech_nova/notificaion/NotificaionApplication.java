478fdb5e30f0cbbb7d514ef3b23fa37bb696d67e
wooseok <wooseok6008@gmail.com>
2024-12-16 13:03:42 +0900

    feat: notification 서버 생성

---
+++ com.tech-nova.notificaion/src/main/java/com/tech_nova/notificaion/NotificaionApplication.java
@@ -0,0 +1,13 @@
+package com.tech_nova.notificaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificaionApplication.class, args);
+	}
+
+}
