565c3c1bb427cc98b3f88f6a2d33d30d5d4d055d
misu <misuhan33@gmail.com>
2024-12-16 20:04:01 +0900

    feat: createDeliveryManager 권한 검증 완료

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryManagerController.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryManagerController.java
@@ -17,14 +17,22 @@ public class DeliveryManagerController {
     private final DeliveryManagerService deliveryManagerService;
 
     @PostMapping
-    public ResponseEntity<ApiResponseDto<UUID>> createDeliveryManager(@RequestBody DeliveryManagerRequest request) {
-        UUID deliveryManagerId = deliveryManagerService.createDeliveryManager(request.toDTO());
+    public ResponseEntity<ApiResponseDto<UUID>> createDeliveryManager(
+            @RequestBody DeliveryManagerRequest request,
+            @RequestHeader(value = "X-User-Id", required = true) UUID userId,
+            @RequestHeader(value = "X-Role", required = true) String role
+    ) {
+        UUID deliveryManagerId = deliveryManagerService.createDeliveryManager(request.toDTO(), userId, role);
         return ResponseEntity.ok(ApiResponseDto.success("Delivery Manager created successfully", deliveryManagerId));
     }
 
     @GetMapping("/{delivery_manager_id}")
-    public ResponseEntity<ApiResponseDto<DeliveryManagerResponse>> createDeliveryManager(@PathVariable("delivery_manager_id") UUID deliveryManagerId) {
-        DeliveryManagerResponse deliveryManager = deliveryManagerService.getDeliveryManager(deliveryManagerId);
+    public ResponseEntity<ApiResponseDto<DeliveryManagerResponse>> createDeliveryManager(
+            @PathVariable("delivery_manager_id") UUID deliveryManagerId,
+            @RequestHeader(value = "X-User-Id", required = true) UUID userId,
+            @RequestHeader(value = "X-Role", required = true) String role
+    ) {
+        DeliveryManagerResponse deliveryManager = deliveryManagerService.getDeliveryManager(deliveryManagerId, userId, role);
         return ResponseEntity.ok(ApiResponseDto.success("Delivery Manager created successfully", deliveryManager));
     }
 }
