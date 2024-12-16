ad22fdc71c82a29ca34bc07f6c85d5d7b0fdd74e
misu <misuhan33@gmail.com>
2024-12-17 01:24:58 +0900

    feat: swagger opertaion 적용

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryRouteRecordController.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/presentation/controller/DeliveryRouteRecordController.java
@@ -6,6 +6,7 @@ import com.tech_nova.delivery.application.service.DeliveryService;
 import com.tech_nova.delivery.presentation.dto.ApiResponseDto;
 import com.tech_nova.delivery.presentation.request.DeliveryRouteSearchRequest;
 import com.tech_nova.delivery.presentation.request.DeliveryRouteUpdateRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +23,8 @@ public class DeliveryRouteRecordController {
     private final DeliveryService deliveryService;
     private final DeliveryRouteRecordService deliveryRouteRecordService;
 
+    @Operation(summary = "경로 기록 단건 조회",
+            description = "마스터와 허브 관리자는 삭제 처리된 기록도 조회 가능")
     @GetMapping("/{delivery_route_id}")
     public ResponseEntity<ApiResponseDto<DeliveryRouteRecordResponse>> getDelivery(
             @PathVariable("delivery_route_id") UUID deliveryRouteId,
@@ -32,6 +35,8 @@ public class DeliveryRouteRecordController {
         return ResponseEntity.ok(ApiResponseDto.success("Delivery created successfully", routeRecord));
     }
 
+    @Operation(summary = "경로 기록 수정",
+            description = "여러 데이터를 한번에 조작할 수 있는 위험으로 마스터만 권한 부여")
     @PatchMapping("/{delivery_route_id}")
     public ResponseEntity<ApiResponseDto<UUID>> updateDeliveryRoute(
             @PathVariable("delivery_route_id") UUID deliveryRouteId,
@@ -43,6 +48,7 @@ public class DeliveryRouteRecordController {
         return ResponseEntity.ok(ApiResponseDto.success("Delivery route updated successfully", routeRecordId));
     }
 
+    @Operation(summary = "경로 기록 상태 수정")
     @PatchMapping("/{delivery_route_id}/status")
     public ResponseEntity<ApiResponseDto<UUID>> updateDeliveryRouteStatus(
             @PathVariable("delivery_route_id") UUID deliveryRouteId,
@@ -54,6 +60,7 @@ public class DeliveryRouteRecordController {
         return ResponseEntity.ok(ApiResponseDto.success("Delivery route status updated successfully", routeRecordId));
     }
 
+    @Operation(summary = "경로 기록 배송 담당자 수정")
     @PatchMapping("/{delivery_route_id}/delivery-managers")
     public ResponseEntity<ApiResponseDto<UUID>> updateDeliveryRouteStatus(
             @PathVariable("delivery_route_id") UUID deliveryRouteId,
@@ -65,6 +72,7 @@ public class DeliveryRouteRecordController {
         return ResponseEntity.ok(ApiResponseDto.success("Delivery route status updated successfully", routeRecordId));
     }
 
+    @Operation(summary = "경로 기록 삭제")
     @DeleteMapping("/{delivery_route_id}")
     public ResponseEntity<ApiResponseDto<Void>> deleteDeliveryRoute(
             @PathVariable("delivery_route_id") UUID deliveryRouteId,
@@ -75,6 +83,7 @@ public class DeliveryRouteRecordController {
         return ResponseEntity.ok(ApiResponseDto.success("Delivery route deleted successfully"));
     }
 
+    @Operation(summary = " 경로 기록 검색 조회")
     @GetMapping
     public ResponseEntity<ApiResponseDto<Page<DeliveryRouteRecordResponse>>> getAllCompanyRouteRecords(
             DeliveryRouteSearchRequest deliveryRouteSearchRequest,
