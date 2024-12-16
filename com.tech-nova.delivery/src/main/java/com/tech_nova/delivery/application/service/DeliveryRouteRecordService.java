9ff0fd27673d2dc77cacc569381ccd89c0ba454b
misu <misuhan33@gmail.com>
2024-12-17 00:43:56 +0900

    feat: 배송 및 배송 경로 기록, 업체 배송 경로 기록 수정 권한 검증 추가
    - 허브 관리자는 담당 허브만 수정 가능
    - 배송 담당자는 본인 배송에 대해서만 수정 가능

--- com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/application/service/DeliveryRouteRecordService.java
+++ com.tech-nova.delivery/src/main/java/com/tech_nova/delivery/application/service/DeliveryRouteRecordService.java
@@ -1,7 +1,9 @@
 package com.tech_nova.delivery.application.service;
 
 import com.tech_nova.delivery.application.dto.res.DeliveryRouteRecordResponse;
+import com.tech_nova.delivery.application.dto.res.HubResponseDto;
 import com.tech_nova.delivery.domain.model.delivery.Delivery;
+import com.tech_nova.delivery.domain.model.delivery.DeliveryCompanyRouteRecord;
 import com.tech_nova.delivery.domain.model.delivery.DeliveryRouteRecord;
 import com.tech_nova.delivery.domain.model.delivery.DeliveryStatus;
 import com.tech_nova.delivery.domain.model.manager.DeliveryManager;
@@ -9,6 +11,7 @@ import com.tech_nova.delivery.domain.model.manager.DeliveryManagerRole;
 import com.tech_nova.delivery.domain.repository.DeliveryManagerRepository;
 import com.tech_nova.delivery.domain.repository.DeliveryRouteRecordRepository;
 import com.tech_nova.delivery.domain.repository.DeliveryRouteRecordRepositoryCustom;
+import com.tech_nova.delivery.infrastructure.dto.HubSearchDto;
 import com.tech_nova.delivery.presentation.request.DeliveryRouteSearchRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,13 +21,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @EnableScheduling
 public class DeliveryRouteRecordService {
-    private final AuthService authService;
+    private final HubService hubService;
 
     private final DeliveryManagerRepository deliveryManagerRepository;
     private final DeliveryRouteRecordRepository deliveryRouteRecordRepository;
@@ -51,10 +57,22 @@ public class DeliveryRouteRecordService {
 
     @Transactional
     public UUID updateRouteRecordDeliveryManager(UUID deliveryRouteId, UUID deliveryManagerId, UUID userId, String role) {
+        if (role.equals("COMPANY_MANAGER") || role.equals("HUB_DELIVERY_MANAGER")) {
+            throw new IllegalArgumentException("수정 권한이 없습니다.");
+        }
+
         DeliveryRouteRecord routeRecord = deliveryRouteRecordRepository.findById(deliveryRouteId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 배송 경로를 찾을 수 없습니다."));
 
         Delivery delivery = routeRecord.getDelivery();
+
+        if (role.equals("HUB_MANAGER")) {
+            validateManagedHub(delivery, userId);
+        }
+
+        if (role.equals("COMPANY_DELIVERY_MANAGER")) {
+            validateDeliveryManagerManagedDelivery(delivery.getCompanyRouteRecords(), userId);
+        }
 
         if (delivery.getCurrentStatus().equals(DeliveryStatus.DELIVERY_COMPLETED)) {
             throw new IllegalArgumentException("완료된 배송은 배송 담당자를 변경할 수 없습니다.");
@@ -79,5 +97,37 @@ public class DeliveryRouteRecordService {
 
         Delivery delivery = routeRecord.getDelivery();
         delivery.deleteRouteRecordState(deliveryRouteId, userId);
+    }
+
+
+    private void validateManagedHub(Delivery delivery, UUID userId) {
+        List<UUID> hubIdList = Optional.ofNullable(hubService.getHubs(new HubSearchDto(), "MASTER", 0, 10).getData())
+                .map(hubsPage -> hubsPage.getContent())
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(hub -> hub.getHubManagerId().equals(userId))
+                .map(HubResponseDto::getHubId)
+                .toList();
+
+        if (!hubIdList.contains(delivery.getDepartureHubId()) && !hubIdList.contains(delivery.getArrivalHubId())) {
+            throw new IllegalArgumentException("권한이 없습니다.");
+        }
+    }
+
+    private void validateDeliveryManagerManagedDelivery(List<DeliveryCompanyRouteRecord> companyRouteRecords, UUID userId) {
+        boolean hasPermission = false;
+
+
+        for (DeliveryCompanyRouteRecord companyRouteRecord : companyRouteRecords) {
+            if (companyRouteRecord.getDeliveryManager() != null && companyRouteRecord.getDeliveryManager().getId().equals(userId)) {
+                hasPermission = true;
+                break;
+            }
+        }
+
+
+        if (!hasPermission) {
+            throw new IllegalArgumentException("권한이 없습니다.");
+        }
     }
 }
