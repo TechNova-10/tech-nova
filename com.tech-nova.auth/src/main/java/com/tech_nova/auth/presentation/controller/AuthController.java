9ec490d9f61248dd373bda39c3f6a76b3ef1b218
misu <misuhan33@gmail.com>
2024-12-17 10:13:31 +0900

    chore: auth zipkin 및 swagger 적용

--- com.tech-nova.auth/src/main/java/com/tech_nova/auth/presentation/controller/AuthController.java
+++ com.tech-nova.auth/src/main/java/com/tech_nova/auth/presentation/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.tech_nova.auth.presentation.dto.ApiResponseDto;
 import com.tech_nova.auth.presentation.dto.AuthResponseDto;
 import com.tech_nova.auth.presentation.request.SignInRequestDto;
 import com.tech_nova.auth.presentation.request.UserRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +21,14 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인")
     @PostMapping("/signIn")
     public ResponseEntity<?> signIn(@RequestBody SignInRequestDto signInRequest) {
         String token = authService.signIn(signInRequest.getUsername(), signInRequest.getPassword());
         return ResponseEntity.ok(new AuthResponseDto(token));
     }
 
+    @Operation(summary = "회원가입")
     @PostMapping("/signUp")
     public ResponseEntity<ApiResponseDto<Void>> signUp(@RequestBody UserRequest request) {
         authService.signUp(request.toDTO());
